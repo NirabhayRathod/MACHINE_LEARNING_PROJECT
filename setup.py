712833331cb94a789ede52cdbb703339fdbcec0b
NirabhayRathod <nirbhay105633016@gmail.com>
2025-08-21 00:13:49 +0530

    setup file modified

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from typing import List
 
 c='-e .'
 def function(file_path:str)->List[str]:
+    '''this function return list of requirements '''
     requirements=[]
     with open(file_path) as file:
         requirements=file.readlines()
+        '''when we read lines , \n also come , to remove that we use below code'''
         requirements=[i.replace('\n','') for i in requirements]
         
-        if c in requirements:
+        if c in requirements:  # we dont want -e. here , so we remove it
             requirements.remove(c)
     return requirements
 
